f61ed489693f8740ad32a9d005e19084db564bb6
Cam-4587 Conroy <cameronconroy@live.com>
2024-12-19 14:13:59 +0000

    update website

---
+++ subscribe.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("thanks");
+const successMessage = document.getElementById("congo");
+
+form.addEventListener("submit", () => {
+    window.alert("Thank you for Subscribing, you will shorly receive an issue confirming your subscription.");
+});
